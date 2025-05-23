bf69d8ed250cb4bbe6a7a1eef8035b2d24fc3616
federico caria <fred.robicheaux@gmail.com>
2025-05-23 11:16:30 +0200

    secret key

--- app/init.py
+++ app/init.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
-    
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-secret-key')
+
     from app.blog.routes import blog_bp
     app.register_blueprint(blog_bp)
-    
+
     return app
